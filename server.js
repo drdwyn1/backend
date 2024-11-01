9efc481b918bf2c54e931271ef418c9564591898
Aditya Ranjan <drdwyn1@gmail.com>
2024-11-01 20:03:59 +0530

    final

--- server.js
+++ server.js
@@ -80,6 +80,6 @@ app.post("/final-mail", async (req, res) => {
   res.send("Email sent");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server started on port 3000");
 });
